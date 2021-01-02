d52a8f9b219b383526f97857f94528549d31da4a
Monumma <teobar91@gmail.com>
2021-01-02 11:22:40 +0100

    this just changes all instances of feet to meters. It doesn't work on the character page

---
+++ beyondmetric.js
@@ -0,0 +1,3 @@
+document.body.innerHTML = document.body.innerHTML.replace(/ft./g, "m");
+document.body.innerHTML = document.body.innerHTML.replace(/feet/g, "meters");
+document.body.innerHTML = document.body.innerHTML.replace(/foot/g, "meter");
